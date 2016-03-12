13210b6cc1195e9793dd03f36c0b235303f3a50b
danilofernandes45 <danilofernandes45@gmail.com>
2016-03-12 11:54:37 -0300

    Commit inicial

---
+++ Guia/app/src/main/java/com/example/android/guiadocientistamirim/LicaoDoMal1.java
@@ -0,0 +1,13 @@
+package com.example.android.guiadocientistamirim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LicaoDoMal1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licao_do_mal1);
+    }
+}

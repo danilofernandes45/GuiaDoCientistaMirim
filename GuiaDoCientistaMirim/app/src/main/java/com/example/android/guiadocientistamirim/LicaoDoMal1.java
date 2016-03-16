5c046bf4e6b604f94ebc1631c925737c1d5887b2
Danilo <danilofernandes45@gmail.com>
2016-03-15 21:11:17 -0300

    NextLesson implementado

--- GuiaDoCientistaMirim/app/src/main/java/com/example/android/guiadocientistamirim/LicaoDoMal1.java
+++ GuiaDoCientistaMirim/app/src/main/java/com/example/android/guiadocientistamirim/LicaoDoMal1.java
@@ -1,9 +1,13 @@
 package com.example.android.guiadocientistamirim;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+
 
 public class LicaoDoMal1 extends AppCompatActivity {
 
@@ -14,8 +18,20 @@ public class LicaoDoMal1 extends AppCompatActivity {
     }
 
     public void cumprirLicao(View view){
-        Intent it = new Intent(this, LicaoDoMal2.class);
-        startActivity(it);
+
+        AlertDialog.Builder exp = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        exp.setView(inflater.inflate(R.layout.explicacao_1, null))
+                .setPositiveButton(R.string.next_lesson, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent it = new Intent(getApplicationContext(), LicaoDoMal2.class);
+                        startActivity(it);
+                    }
+                })
+                .setNegativeButton(R.string.back, null);
+
+        exp.show();
     }
 
     public void voltarInicio(View view){
--- GuiaDoCientistaMirim/app/src/main/java/com/example/android/guiadocientistamirim/LicaoDoMal2.java
+++ GuiaDoCientistaMirim/app/src/main/java/com/example/android/guiadocientistamirim/LicaoDoMal2.java
@@ -1,8 +1,11 @@
 package com.example.android.guiadocientistamirim;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class LicaoDoMal2 extends AppCompatActivity {
@@ -14,8 +17,18 @@ public class LicaoDoMal2 extends AppCompatActivity {
     }
 
     public void cumprirLicao(View view){
-        Intent it = new Intent(this, LicaoDoMal3.class);
-        startActivity(it);
+
+        AlertDialog.Builder exp = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        exp.setView(inflater.inflate(R.layout.explicacao_2, null))
+                .setPositiveButton(R.string.next_lesson, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent it = new Intent(getApplicationContext(), LicaoDoMal3.class);
+                        startActivity(it);
+                    }
+                })
+                .setNegativeButton(R.string.back, null);
     }
 
     public void voltarInicio(View view){
